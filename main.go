cad7e72939136491d37e204a513650d4e97b0942
Bayu Ilham <bayu.ilhamendianto@gmail.com>
2021-08-05 10:15:35 +0700

    add some dummy message

--- main.go
+++ main.go
@@ -60,17 +60,41 @@ func biller(w http.ResponseWriter, r *http.Request) {
 
 	var msgID string
 	var fileName string
-
+	var BzID string
 	msgID = fmt.Sprintf("%v", request.BusMsg.AppHdr.MessageDefinitionIdentifier)
+	BzID = fmt.Sprintf("%v", request.BusMsg.AppHdr.BusinessMessageIdentifier)
 	log.Println("MsgDefIdn:", msgID)
 
 	switch msgID {
 	case "pacs.008.001.08":
+		switch BzID {
+		case "20210301INDOIDJA010ORB12356789":
+			fileName = "sample_AccountEnquiry_AccountID_pacs.002_response_CIHUB_to_OFI.xml.json"
+		case "20210301INDOIDJA010ORB12357890":
+			fileName = "sample_AccountEnquiry_AccountID_pacs.002_response_CIHUB_to_OFI.xml.json"
+		default:
+			fileName = "sample_AccountEnquiry_AccountID_pacs.002_response_CIHUB_to_OFI.xml.json"
+		}
 		fileName = "sample_CreditTransfer_AccountID_pacs.002_response_CIHUB_to_OFI.xml.json"
 	case "pacs.009.001.09":
 		fileName = "sample_FItoFICreditTransfer_pacs.002_response_CIHUB_to_OFI.xml.json"
+	case "pacs.028.001.04":
+		fileName = "PaymentStatusReqResponse.json"
 	case "prxy.001.001.01":
-		fileName = "sample_prxy.002_response_alias_mgmt_NEWR_CIHUB_to_OFI.xml.json"
+		switch BzID {
+		case "20210301INDOIDJA610ORB11111111":
+			fileName = "sample_prxy.002_dummyDeregist.json"
+		case "20210301INDOIDJA610ORB22222222":
+			fileName = "sample_prxy.002_dummyUpdate.json"
+		case "20210301INDOIDJA610ORB33333333":
+			fileName = "sample_prxy.002_dummySuspend.json"
+		case "20210301INDOIDJA610ORB44444444":
+			fileName = "sample_prxy.002_dummyActivation.json"
+		case "20210301INDOIDJA610ORB55555555":
+			fileName = "sample_prxy.002_dummyPorting.json"
+		default:
+			fileName = "sample_prxy.002_response_alias_mgmt_NEWR_CIHUB_to_OFI.xml.json"
+		}
 	case "prxy.003.001.01":
 		fileName = "sample_prxy.004_response_alias_resolution_CIHUB_to_OFI.xml.json"
 	case "prxy.005.001.01":
@@ -113,4 +137,22 @@ type ChannelInput struct {
 type BusMsg struct {
 	AppHdr   *head.BusinessApplicationHeaderV01 `xml:"AppHdr" json:"AppHdr"`
 	Document json.RawMessage                    `xml:"Document" json:"Document"`
+}
+
+func getFileName(BzID string) (fileName string) {
+	switch BzID {
+	case "20210301INDOIDJA610ORB11111111":
+		fileName = "sample_prxy.002_dummyDeregist.json"
+	case "20210301INDOIDJA610ORB22222222":
+		fileName = "sample_prxy.002_dummyUpdate.json"
+	case "20210301INDOIDJA610ORB33333333":
+		fileName = "sample_prxy.002_dummySuspend.json"
+	case "20210301INDOIDJA610ORB44444444":
+		fileName = "sample_prxy.002_dummyActivation.json"
+	case "20210301INDOIDJA610ORB55555555":
+		fileName = "sample_prxy.002_dummyPorting.json"
+	default:
+		fileName = "sample_prxy.002_response_alias_mgmt_NEWR_CIHUB_to_OFI.xml.json"
+	}
+	return fileName
 }
