04a7b721365178d9fad7f811d2eb12e2e332e4b5
Bayu Ilham <bayu.ilhamendianto@gmail.com>
2021-10-04 19:11:00 +0700

    renew response sample messages

--- main.go
+++ main.go
@@ -60,50 +60,36 @@ func biller(w http.ResponseWriter, r *http.Request) {
 
 	var msgID string
 	var fileName string
-	var BzID string
 	msgID = fmt.Sprintf("%v", *request.BusMsg.AppHdr.MessageDefinitionIdentifier)
-	BzID = fmt.Sprintf("%v", request.BusMsg.AppHdr.BusinessMessageIdentifier)
 	log.Println("MsgDefIdn:", msgID)
 
 	switch msgID {
 	case "pacs.008.001.08":
-		switch BzID {
-		case "20210301INDOIDJA010ORB12356789":
-			fileName = "sample_AccountEnquiry_AccountID_pacs.002_response_CIHUB_to_OFI.xml.json"
-		case "20210301INDOIDJA010ORB12357890":
-			fileName = "sample_AccountEnquiry_AccountID_pacs.002_response_CIHUB_to_OFI.xml.json"
-		case "20210301INDOIDJA510ORB12345678":
-			fileName = "sample_pacs.002_response_account_enquiry.json"
-		default:
-			fileName = "sample_pacs.002_response_account_enquiry.json"
-		}
-		fileName = "sample_CreditTransfer_AccountID_pacs.002_response_CIHUB_to_OFI.xml.json"
+		// switch Bz{
+		// creditTransfer
+		// fileName = "sample_CreditTransfer_AccountID_pacs.008_request_OFI_to_CIHUB.xml.json"
+		//
+		// default:
+		//accountInquiry response
+		fileName = "sample_pacs.002_response_account_enquiry.json"
+	// }
+	case "prxy.001.001.01":
+		//proxyManagement
+		fileName = "sample_prxy.002_response_alias_mgmt_NEWR_CIHUB_to_OFI.xml copy.json"
 	case "pacs.009.001.09":
+		//FItoFI
 		fileName = "sample_FItoFICreditTransfer_pacs.002_response_CIHUB_to_OFI.xml.json"
 	case "pacs.028.001.04":
+		//PaymentStatus Request
 		fileName = "PaymentStatusReqResponse.json"
-	case "prxy.001.001.01":
-		fileName = "sample_prxy.002_response_alias_mgmt_NEWR_CIHUB_to_OFI.xml copy.json"
-		// switch BzID {
-		// case "20210301INDOIDJA610ORB11111111":
-		// 	fileName = "sample_prxy.002_dummyDeregist.json"
-		// case "20210301INDOIDJA610ORB22222222":
-		// 	fileName = "sample_prxy.002_dummyUpdate.json"
-		// case "20210301INDOIDJA610ORB33333333":
-		// 	fileName = "sample_prxy.002_dummySuspend.json"
-		// case "20210301INDOIDJA610ORB44444444":
-		// 	fileName = "sample_prxy.002_dummyActivation.json"
-		// case "20210301INDOIDJA610ORB55555555":
-		// 	fileName = "sample_prxy.002_dummyPorting.json"
-		// default:
-		// 	fileName = "sample_prxy.002_response_alias_mgmt_NEWR_CIHUB_to_OFI.xml.json"
-		// }
 	case "prxy.003.001.01":
+		//Alias Resolution
 		fileName = "sample_prxy.004_response_alias_resolution_CIHUB_to_OFI.xml.json"
 	case "prxy.005.001.01":
+		//Alias Registration
 		fileName = "sample_prxy.006_response_alias_enquiry_CIHUB_to_OFI.xml.json"
-	default:
-		fileName = "sample_pacs.002_response_account_enquiry.json"
+		// default:
+		// 	fileName = "sample_pacs.002_response_account_enquiry.json"
 	}
 
 	//fmt.Println("Enter file name: ")
